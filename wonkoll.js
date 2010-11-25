1e88ff320dec624c6b4513ff7b8bf69463247ad3
Wes Garland <wes@page.ca>
2010-11-25 15:14:27 -0500

    Added enforced-serial module.provide to LazyLoad.js loader

--- wonkoll.js
+++ wonkoll.js
@@ -21,6 +21,7 @@ var loading;
 if (module.constructor.prototype.load === null)
   throw new Error("Sorry, your CommonJS environment does not support alternate module loaders");
 
+/** Load a module via LazyLoad.js */
 module.constructor.prototype.load = function wonko_load(moduleIdentifier, callback)
 {
   var URL = require.canonicalize(moduleIdentifier) + "?1";
@@ -33,10 +34,13 @@ module.constructor.prototype.load = function wonko_load(moduleIdentifier, callba
   LazyLoad.js(URL);
 }
 
+/** Process the module declaring using identity information provided by module.load */
 module.constructor.prototype.declare = function wonko_declare(dependencies, moduleFactory)
 {
-  var id = loading.id;
-  var callback = loading.callback;
+  var id 	= loading.id;
+  var callback  = loading.callback;
+  var deps	= [], i, label;
+
   loading = void 0;
 
   if (typeof dependencies === "function")
@@ -46,7 +50,52 @@ module.constructor.prototype.declare = function wonko_declare(dependencies, modu
   }
 
   require.memoize(id, dependencies, moduleFactory);
-  module.provide(dependencies, callback);
+
+  /* Build a list of dependencies suitable for module.provide; this
+   * means no labeled dependencies. 
+   */
+  function addDep(dependency)
+  {
+    var id = require.id(dependency);
+
+    if (require.isMemoized(id) || deps.indexOf(id) !== -1)
+      return;
+
+    deps.push(id);
+  }
+
+  for (i=0; i < dependencies.length; i++)
+  {
+    if (typeof dependencies[i] === "string")
+      addDep(dependencies[i]);
+    else
+    {
+      for (label in dependencies[i])
+	addDep(dependencies[i][label])
+    }
+  }
+
+  module.provide(deps, callback);
+}
+
+/** LazyLoad.js does not support parallel load operations, so we replace 
+ *  module.provide with a serial resolver incase the underlying environment 
+ *  uses a parallel algorithm.
+ */
+module.constructor.prototype.provide = function wonko_provide(dependencies, callback)
+{
+  function provide(module, dependencies, callback)
+  {
+    if (dependencies.length === 0)
+    {
+      callback();
+      return;
+    }
+
+    module.load(dependencies[0], function() { module.provide(dependencies.slice(1), callback) });
+  }
+
+  provide(this, dependencies, callback);
 }
 
 })();
