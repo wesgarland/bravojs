bededdd79f596b2e93ba259c5c0a855adbf37cb5
Wes Garland <wes@page.ca>
2018-06-01 14:50:54 -0400

    Added BravoJS/Node module compatibility shim

---
+++ utility/cjs2-node.js
@@ -0,0 +1,31 @@
+/**
+ *  @file       cjs2-node.js            Compatibility layer for Node.js which allows
+ *                                      us to execute basic CommonJS Modules/2.0-d8
+ *                                      modules within the Node environment.
+ *  Usage:  require("./cjs2") 
+ *          in the main program as early as possible (before any calls to
+ *          module.declare or require)
+ *
+ *  @author     Wes Garland, wgarland@sparc.network
+ *  @date       May 2018
+ */
+
+/** Module.declare de-sugar for any modules which are CommonJS Modules/2.0 */
+module.constructor.prototype.declare = function cjs2_moduleDeclare(deps, cjs1_moduleFun)
+{
+  if (arguments.length === 1)
+  {
+    cjs1_moduleFun = deps;
+    deps = undefined;
+  }
+
+  function newRequire(moduleName)
+  {
+    if (moduleName[0] != ".")
+      return require(moduleName);
+    return require(newRequire.path + moduleName);
+  }
+  newRequire.path = (this.id||"").replace(/[^\/]*$/,"");
+
+  cjs1_moduleFun(newRequire, this.exports, this);   
+}
